8837ee11bfd260b385e959f02d2cc849cd0f0044
Neirameta <matrixolo.id@gmail.com>
2024-10-28 18:54:00 +0700

    Add home file

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+```javascript
+export default function Home() {
+  return (
+    <div>
+      <h1>LifeEnergy</h1>
+      <p>Welcome to LifeEnergy - Your personal source of energy and inspiration</p>
+    </div>
+  )
+}
+```
